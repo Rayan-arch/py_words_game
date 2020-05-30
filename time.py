ffe1d14d862b7389b86e6eae02cb194dc04f3c55
Your Name <you@example.com>
2020-05-30 23:05:21 +0200

    Added more words and a random funtion for random generate words

--- time.py
+++ time.py
@@ -1,5 +1,6 @@
 import time as t
 import matplotlib.pyplot as plt
+import random as r
 
 #This program have been created for practice writing code in python
 #Exercise from Udemy:
@@ -8,34 +9,44 @@ import matplotlib.pyplot as plt
 #Create program to help the user type faster. Give him a word and ask him
 #to write five times. Check how manny secounds it took him to type the word
 #at each round.
-print("Program to practice speed up typing words")
-print("Try to type 'wodosplad' five times and do not make mistakes")
-ready=input("Can we start?(Y?N)").lower()
+
 y=[]
 x=[1,2,3,4,5]
+mistakes=0
+wodospad=['wodospad','szkola','co','kto','mama','lucyna','przedszkole']
+word_set = r.randrange(len(wodospad)+1)
+
+print("Program to practice speed up typing words")
+print("Try to type '"+wodospad[word_set]+"' five times and do not make mistakes")
+ready=input("Can we start?(Y?N)").lower()
+
 if ready == "y":
     print("start!")
     for fast in range(1,6):
-        t.sleep(4)
+        t.sleep(2)
         start=t.time()
         word=input("Round "+str(fast)+": ")
         end=t.time()
         time=end-start
         y.append(time)
-        if word == 'wodospad':
+        if word == wodospad[word_set]:
             print("Good work!\nYou can do it faster!")
         else:
             print("What a shame.\nTry again.")
-        print("It took you {} sec.".format(round(time,2)
-                                       ))
-    print("You did it five times, how it feels?\n Let see your results!")
+            mistakes += 1
+        print("It took you {} sec.".format(round(time,2)))
+    print("You did {} mistake(s)".format(mistakes))
+    print("You did it five times, how it feels?\nLet see your results!")
+    
 
 #And now show it on plot!
 
     plt.plot(x,y)
-    plt.ylabel("sec")
-    plt.xlabel("round")
-    plt.title("Your Score")
+    legend=['1','2','3','4','5']
+    plt.xticks(x,legend)
+    plt.ylabel("Time (sec)")
+    plt.xlabel("Attempts")
+    plt.title("Your typing evolution")
     t.sleep(3)
     plt.show()
 else:
